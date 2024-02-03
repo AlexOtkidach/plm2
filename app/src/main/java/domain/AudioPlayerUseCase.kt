f84a1471cf1d616c7dc0987b3a385e8654cbfa84
//AlexOtkidach <132559666+AlexOtkidach@users.noreply.github.com>
2024-02-03 16:41:36 +0300

    1.Разнес код по пакетам presentation, data — по смыслу. 2.Организовал связь между слоями посредством интерфейсов +

--- app/src/main/java/domain/AudioPlayerUseCase.kt
+++ app/src/main/java/domain/AudioPlayerUseCase.kt
@@ -7,23 +7,16 @@ class AudioPlayerUseCase(private val repository: AudioPlayerRepository) : AudioP
     private var isPlaying: Boolean = false
 
     override fun playTrack(track: Track): Boolean {
-        // Ваша реализация воспроизведения трека здесь, например, с использованием MediaPlayer
-        // Верните true, если трек успешно начат воспроизводиться, иначе false
-        // Обновите флаг isPlaying
         isPlaying = true
         return true
     }
 
     override fun pauseTrack(): Boolean {
-        // Ваша реализация паузы воспроизведения здесь
-        // Верните true, если трек успешно поставлен на паузу, иначе false
-        // Обновите флаг isPlaying
         isPlaying = false
         return true
     }
 
     override fun isPlaying(): Boolean {
-        // Верните текущее состояние воспроизведения (играет ли трек)
         return isPlaying
     }
 }
